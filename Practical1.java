27b57936240c85641b0dac13cc0e649c896a6552
saravanakumar0000 <176639872+Saravanakumar0000@users.noreply.github.com>
2026-08-10 08:15:45 +0000

    1

--- Practical1.java
+++ Practical1.java
@@ -74,7 +74,8 @@ class Square extends Rectangle {
     }
 }
 
-public class Practical1 {
+public class Practical1 
+ {
     public static void main(String[] args) {
         Point p1 = new Point(0, 0);
         Point p2 = new Point(4, 0);
